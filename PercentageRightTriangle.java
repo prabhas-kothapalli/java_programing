d0e40410b73ad1c7945dadd8202ad42c2ebfa9b2
prabhas satya <105013662+prabhas-kothapalli@users.noreply.github.com>
2023-03-23 10:43:56 +0530

    Add files via upload

---
+++ PercentageRightTriangle.java
@@ -0,0 +1,27 @@
+package programs;
+
+public class PercentageRightTriangle {
+        public static void rightTriangle(int n)
+        {
+            int i, j;
+            for(i=0; i<n; i++)
+            {
+                for(j=0; j<=i; j++)
+                {
+                    System.out.print("% ");
+                }
+                for(j=2*(n-i); j>0; j--)
+                {
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+        public static void main(String args[])
+        {
+            int n = 5;
+            rightTriangle(n);
+        }
+    }
+
+
